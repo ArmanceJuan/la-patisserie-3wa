96670496615fad158481393a797823f27fd026ad
Laura Sdt <lauramarie.sdt@gmail.com>
2024-09-10 12:05:19 +0200

    create login form

--- src/Routing/Login.jsx
+++ src/Routing/Login.jsx
@@ -1,7 +1,48 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess, loginFailure } from "../slices/authSlice";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.auth?.error);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (username === "admin" && password === "admin") {
+      dispatch(loginSuccess({ username }));
+    } else {
+      dispatch(loginFailure("Nom d'utilisateur ou mot de passe incorrect"));
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Nom d'utilisateur:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Mot de passe:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Se connecter</button>
+      </form>
+      {error && <p>{error}</p>}
     </div>
   );
 };
--- src/components/store.jsx
+++ src/components/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import gameSlice from "../slices/gameSlice.jsx";
 import diceSlice from "../slices/diceSlice.jsx";
+import authSlice from "../slices/authSlice.jsx";
 
 export const store = configureStore({
   reducer: {
